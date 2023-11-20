c5bbb32cf35c0df4f7b839c3cd738dff51d84c60
Kawthar Alkhateeb <kawtharnaser.alkha@gmail.com>
2023-11-19 23:22:50 -0600

    Update FlutterMap

--- SourceCode/SecureShe_App/lib/pages/osm_page.dart
+++ SourceCode/SecureShe_App/lib/pages/osm_page.dart
@@ -1,26 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:url_launcher/url_launcher.dart';
 
+// ignore: use_key_in_widget_constructors
 class MyMapOSM extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('OpenStreetMap in Flutter')),
-        body: FlutterMap(
-          options: MapOptions(
-            center: LatLng(50.4488, -104.6178), // Coordinates for Regina, Saskatchewan, Canada
-            zoom: 13.0,
-          ),
-          layers: [
-            TileLayerOptions(
-              urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-              subdomains: ['a', 'b', 'c'],
+    return FlutterMap(
+      options: const MapOptions(
+        initialCenter: LatLng(50.4488, -104.6178),
+        initialZoom: 9.2,
+        interactiveFlags: InteractiveFlag.all & ~InteractiveFlag.rotate,
+      ),
+      children: [
+        TileLayer(
+          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          userAgentPackageName: 'com.example.app',
+        ),
+        // Additional layers or widgets can be added here
+        RichAttributionWidget(
+          attributions: [
+            TextSourceAttribution(
+              'OpenStreetMap contributors',
+              onTap: () => launchUrl(Uri.parse('https://openstreetmap.org/copyright')),
             ),
           ],
         ),
-      ),
+      ],
     );
   }
 }
