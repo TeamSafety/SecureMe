58a5d51c0661c2ca011ca243b41b513fc7d63d5a
Kawthar Alkhateeb <kawtharnaser.alkha@gmail.com>
2024-01-13 21:15:55 -0600

    Update profile page code

--- SourceCode/SecureShe_App/lib/pages/my_profile.dart
+++ SourceCode/SecureShe_App/lib/pages/my_profile.dart
@@ -2,6 +2,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+//TODO: please format the page! 
+
 final FirebaseAuth _auth = FirebaseAuth.instance;
 final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
