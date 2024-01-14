3630d67777e1521677f3c50e916782e198e958ab
Kawthar Alkhateeb <kawtharnaser.alkha@gmail.com>
2024-01-13 21:13:54 -0600

    Update profile page

--- SourceCode/SecureShe_App/lib/pages/my_profile.dart
+++ SourceCode/SecureShe_App/lib/pages/my_profile.dart
@@ -1,40 +1,126 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+
 final FirebaseAuth _auth = FirebaseAuth.instance;
-// fetching the user's data to display in the profile page. 
-void fetchUserProfile() async {
-  User? user = _auth.currentUser;
+final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  if (user != null) {
-    DocumentSnapshot<Map<String, dynamic>> snapshot =
-        await FirebaseFirestore.instance.collection('Users').doc(user.uid).get();
-
-    // Check if the document exists before accessing its fields
-    if (snapshot.exists) {
-      // Access user profile data
-      String userEmail = snapshot['email'];
-      print(userEmail); 
-      // Access more fields as needed
-      // You can now use userEmail and other fields as needed
-    } else {
-      print('Document does not exist.');
-    }
-  }
-}
-
-
-class MyProfile extends StatelessWidget {
+class MyProfile extends StatefulWidget {
   const MyProfile({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    fetchUserProfile(); 
-    return const Text(
-      "My Profile",
-      textAlign: TextAlign.center,
-    );
-    
-  }
+  _MyProfileState createState() => _MyProfileState();
 }
 
+class _MyProfileState extends State<MyProfile> {
+  String userEmail = "";
+  String username = "";
+
+  @override
+  void initState() {
+    super.initState();
+    fetchUserProfile();
+  }
+
+  void fetchUserProfile() async {
+    User? user = _auth.currentUser;
+
+    if (user != null) {
+      DocumentSnapshot<Map<String, dynamic>> snapshot =
+          await _firestore.collection('Users').doc(user.uid).get();
+
+      if (snapshot.exists) {
+        setState(() {
+          userEmail = snapshot['email'];
+          username = snapshot['username'] ?? ''; // Get username if exists
+        });
+      } else {
+        print('Document does not exist.');
+      }
+    }
+  }
+
+  Future<void> updateUserProfile(String newUsername) async {
+    User? user = _auth.currentUser;
+
+    if (user != null) {
+      // Update user profile in Firestore
+      await _firestore.collection('Users').doc(user.uid).update({
+        'username': newUsername,
+      });
+
+      // Update local state
+      setState(() {
+        username = newUsername;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Profile'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Email: $userEmail',
+              style: const TextStyle(fontSize: 18.0),
+            ),
+            const SizedBox(height: 16.0),
+            Text(
+              'Username: $username',
+              style: const TextStyle(fontSize: 18.0),
+            ),
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                _editUsernameDialog(context);
+              },
+              child: const Text('Edit Username'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> _editUsernameDialog(BuildContext context) async {
+    TextEditingController usernameController = TextEditingController();
+
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Edit Username'),
+          content: TextField(
+            controller: usernameController,
+            decoration: const InputDecoration(hintText: 'Enter new username'),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancel'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                String newUsername = usernameController.text.trim();
+                if (newUsername.isNotEmpty) {
+                  updateUserProfile(newUsername);
+                }
+                Navigator.of(context).pop();
+              },
+              child: const Text('Save'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
--- SourceCode/SecureShe_App/lib/pages/osm_page.dart
+++ SourceCode/SecureShe_App/lib/pages/osm_page.dart
@@ -1,10 +1,121 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:geolocator/geolocator.dart';
+//import 'package:geolocator_apple/geolocator_apple.dart';
+//import 'package:geolocator_android/geolocator_android.dart';
+import 'dart:async';
+import 'dart:io' show Platform;
+import 'package:baseflow_plugin_template/baseflow_plugin_template.dart';
+
+Future<Position> _determinePosition() async {
+  bool serviceEnabled;
+  LocationPermission permission;
+  // Test if location services are enabled.
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    // Location services are not enabled don't continue
+    // accessing the position and request users of the
+    // App to enable the location services.
+    return Future.error('Location services are disabled.');
+  }
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      // Permissions are denied, next time you could try
+      // requesting permissions again (this is also where
+      // Android's shouldShowRequestPermissionRationale
+      // returned true. According to Android guidelines
+      // your App should show an explanatory UI now.
+      return Future.error('Location permissions are denied');
+    }
+  }
+  if (permission == LocationPermission.deniedForever) {
+    // Permissions are denied forever, handle appropriately.
+    return Future.error(
+        'Location permissions are permanently denied, we cannot request permissions.');
+  }
+  // When we reach here, permissions are granted and we can
+  // continue accessing the position of the device.
+
+  final LocationSettings locationSettings = LocationSettings(
+    accuracy: LocationAccuracy.high,
+    distanceFilter: 100,
+  );
+  StreamSubscription<Position> positionStream = Geolocator.getPositionStream(locationSettings: locationSettings).listen(
+          (Position? position) {
+        print(position == null ? 'Unknown' : '${position.latitude.toString()}, ${position.longitude.toString()}');
+      });
+
+  return await Geolocator.getCurrentPosition();
+}
+
+class MyMapOSM extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return FlutterMap(
+      options: const MapOptions(
+        initialCenter: LatLng(50.4488, -104.6178),
+        initialZoom: 9.2,
+        interactiveFlags: InteractiveFlag.all & ~InteractiveFlag.rotate,
+      ),
+      children: [
+        TileLayer(
+          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          userAgentPackageName: 'com.example.app',
+        ),
+        // Additional layers or widgets can be added here
+        //CurrentLocationLayer(),
+        CurrentLocationLayer(
+          alignDirectionOnUpdate: AlignOnUpdate.always, // or AlignOnUpdate.once, AlignOnUpdate.never
+          alignPositionOnUpdate: AlignOnUpdate.always, // or AlignOnUpdate.once, AlignOnUpdate.never
+          style: const LocationMarkerStyle(
+            marker: DefaultLocationMarker(
+              child: Icon(
+                Icons.navigation,
+                color: Colors.white,
+              ),
+            ),
+            markerSize: Size(40, 40),
+            markerDirection: MarkerDirection.heading,
+          ),
+        ),
+        RichAttributionWidget(
+          attributions: [
+            TextSourceAttribution(
+              'OpenStreetMap contributors',
+              onTap: () => launchUrl(Uri.parse('https://openstreetmap.org/copyright')),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+/*Widget build() {
+  return CurrentLocationLayer(
+    followOnLocationUpdate: FollowOnLocationUpdate.always,
+    turnOnHeadingUpdate: TurnOnHeadingUpdate.never,
+    style: LocationMarkerStyle(
+      marker: const DefaultLocationMarker(
+        child: Icon(
+          Icons.navigation,
+          color: Colors.white,
+        ),
+      ),
+      markerSize: const Size(40, 40),
+      markerDirection: MarkerDirection.heading,
+    ),
+  );
+}*/
 
 // ignore: use_key_in_widget_constructors
-class MyMapOSM extends StatelessWidget {
+/*class MyMapOSM extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FlutterMap(
@@ -30,4 +141,4 @@ class MyMapOSM extends StatelessWidget {
       ],
     );
   }
-}
+}*/
