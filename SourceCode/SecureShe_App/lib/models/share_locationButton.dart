e21e0a0c113de9c732730c7a9ca6282ab1537d27
Kawthar Alkhateeb <kawtharnaser.alkha@gmail.com>
2024-03-03 15:20:01 -0600

    Add share location button

--- SourceCode/SecureShe_App/lib/models/share_locationButton.dart
+++ SourceCode/SecureShe_App/lib/models/share_locationButton.dart
@@ -1,14 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:my_app/models/AppVars.dart';
 import 'package:my_app/models/save_location.dart';
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
 final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-class SOSButton extends StatelessWidget {
-  const SOSButton({super.key});
+class shareLocationButton extends StatelessWidget {
+  const shareLocationButton({super.key});
 
   @override
   Widget build(BuildContext context) {
