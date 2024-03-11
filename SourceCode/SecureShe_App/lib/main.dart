135017217df4508a4e8ddb7dee6353d6d63d1b9a
Kawthar Alkhateeb <kawtharnaser.alkha@gmail.com>
2024-03-10 19:27:13 -0600

    Add notifications configuration

--- SourceCode/SecureShe_App/lib/main.dart
+++ SourceCode/SecureShe_App/lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:my_app/pages/login.dart';
 import 'firebase_options.dart';
-import 'package:my_app/pages/main_page.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:rxdart/rxdart.dart';
+
+// used to pass messages from event handler to the UI
+final _messageStreamController = BehaviorSubject<RemoteMessage>();
 
 
 void main() async {
@@ -16,7 +18,6 @@ void main() async {
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   // Request permission
   final messaging = FirebaseMessaging.instance;
-
   final settings = await messaging.requestPermission(
     alert: true,
     announcement: false,
@@ -35,16 +36,28 @@ void main() async {
   // if (kDebugMode) {
   //   print('Registration Token=$token');
   // }
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    if (kDebugMode) {
+      print('Handling a foreground message: ${message.messageId}');
+      print('Message data: ${message.data}');
+      print('Message notification: ${message.notification?.title}');
+      print('Message notification: ${message.notification?.body}');
+    }
+    _messageStreamController.sink.add(message);
+  });
   runApp(const MyApp());
 }
 // Background message handler
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  print("Handling a background message: ${message.messageId}");
-  // Handle the background message
-  
+  await Firebase.initializeApp();
+
+  if (kDebugMode) {
+    print("Handling a background message: ${message.messageId}");
+    print('Message data: ${message.data}');
+    print('Message notification: ${message.notification?.title}');
+    print('Message notification: ${message.notification?.body}');
+  }
 }
-
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
