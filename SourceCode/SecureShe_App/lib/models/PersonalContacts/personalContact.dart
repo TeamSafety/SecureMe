0daa2f638671b080549411d7a9a862642fe426ef
Charles Samonte <cksamonte@gmail.com>
2024-04-04 14:50:58 -0600

    Minor changes.

---
+++ SourceCode/SecureShe_App/lib/models/PersonalContacts/personalContact.dart
@@ -0,0 +1,25 @@
+class PersonalContactModel {
+  final String contactName;
+  final String initialsTemp;
+  final String imagePath; 
+  final String addedContactUid; 
+  final String uid;
+
+  PersonalContactModel({
+    required this.contactName,
+    required this.initialsTemp,
+    required this.uid,
+    required this.imagePath, 
+    required this.addedContactUid, 
+  });
+
+  factory PersonalContactModel.fromMap(Map<String, dynamic> map) {
+    return PersonalContactModel(
+      contactName: map['contactName'] ?? '',
+      initialsTemp: map['initialsTemp'] ?? '',
+      uid: map['uid'] ?? '',
+      imagePath: map['imagePath'] ?? 'assets/images/avatar_default.jpg',
+      addedContactUid: map['contactUid'] ?? '',
+    );
+  }
+}
